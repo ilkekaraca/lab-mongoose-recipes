2dbfa51cd0604c5df47735d35a41c59d61783242
“Ilke <ilkedidem@gmail.com>
2023-04-20 16:52:56 +0200

    done

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const RecipeModel = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return RecipeModel.deleteMany();
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    const newRecipe = {
+      title: "Pizza",
+      ingredients: ["tomato", "salami", "cheese"],
+      cuisine: "Italian",
+      creator: "Didem",
+    };
+
+    return RecipeModel.create(newRecipe);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((recipe) => {
+    console.log(recipe.title);
+  })
+  .then(() => {
+    return RecipeModel.insertMany(data);
+  })
+  .then((recipes) => {
+    recipes.forEach((rec) => {
+      console.log(rec.title);
+    });
+  })
+  .then(() => {
+    return RecipeModel.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+  })
+  .then(() => {
+    console.log("Success");
+  })
+  .then(() => {
+    return RecipeModel.deleteOne({ title: "Carrot Cake" });
+  })
+  .then(() => {
+    console.log("Delete Cake");
+  })
+  .then(() => {
+    mongoose.disconnect();
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
